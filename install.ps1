5dfab58d7bb4098df24004485cb8a5cf81ac5a91
Rick Sullivan <rick@ricksullivan.net>
2021-06-28 11:41:09 -0700

    Add install script for PowerShell

---
+++ install.ps1
@@ -0,0 +1,4 @@
+winget install Microsoft.dotNetFramework
+winget install "Microsoft.VisualStudio.2022.Enterprise-Preview" --override "--add Microsoft.VisualStudio.Workload.ManagedDesktop --add Microsoft.VisualStudio.Workload.NetWeb --add Microsoft.Net.Component.4.6.2.TargetingPack --includeRecommended"
+winget install vscode
+code --install-extension vscodevim.vim
